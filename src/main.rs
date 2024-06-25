fd532e54e2d81e3ca04f9375117df46632980b1e
Tech13-08 <falaktulsi@gmail.com>
2024-06-24 20:21:49 -0700

    Echo Code

--- src/main.rs
+++ src/main.rs
@@ -18,11 +18,13 @@ fn main() {
             match trimmed_input[0] {
                 "exit 0" => return,
                 "echo" => {
-                    let echo_content = "";
                     if trimmed_input.len() > 1 {
-                        echo_content = &trimmed_input[1..].join(" ");
+                        let echo_content = &trimmed_input[1..].join(" ");
+                        echo(echo_content);
                     }
-                    echo(echo_content);
+                    else{
+                        echo(" ");
+                    }
                 },
                 _ => println!("{}: command not found", &trimmed_input[0]),
             }
