7115f742f53e06b47b3aca0a9703ab13d209e480
Tech13-08 <falaktulsi@gmail.com>
2024-06-24 20:13:00 -0700

    Exit Code

--- src/main.rs
+++ src/main.rs
@@ -13,10 +13,18 @@ fn main() {
         let stdin = io::stdin();
         let mut input = String::new();
         stdin.read_line(&mut input).unwrap();
-        let trimmed_input = input.trim();
-        match trimmed_input {
-            "exit 0" => return,
-            _ => println!("{}: command not found", trimmed_input),
+        let trimmed_input = input.trim().split_whitespace().collect::<Vec<&str>>();
+        if trimmed_input.len() > 0 {
+            match trimmed_input[0] {
+                "exit 0" => return,
+                "echo" => echo(if (trimmed_input.len() > 0) trimmed_input[1..].join(" ") else ""),
+                _ => println!("{}: command not found", trimmed_input),
+            }
         }
     }
+}
+
+
+fn echo(content: &str) {
+    println!("{}", content);
 }
